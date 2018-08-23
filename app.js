337068803289e3cd39d5d83ba4a35cc738800f15
uekichi <41245953+uekichi@users.noreply.github.com>
2018-08-23 20:13:04 +0900

    argvの正体
    授業での補足

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 'use strict';
+console.log('process.argv[0] : ' + process.argv[0]);
+console.log('process.argv[1] : ' + process.argv[1]);
+console.log('process.argv[2] : ' + process.argv[2]);
+
 const number = process.argv[2] || 0;
 let sum = 0;
 for (let i = 1; i <= number; i++) {
-    sum = sum + i;
+  sum += i;
 }
-console.log(sum);
+console.log('１から' + number + 'の合計は　' + sum);
